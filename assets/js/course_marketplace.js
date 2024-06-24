43456eb9f503d6ece4195ac043f151600b24884b
Immanue <aimmanuel925@gmail.com>
2024-06-24 19:59:28 +0000

    update course-marketplace
    change course-Affiliate Marketing Mastery thumbnail.Because it was too small in width.

--- assets/js/course_marketplace.js
+++ assets/js/course_marketplace.js
@@ -28,7 +28,7 @@ const courses = [
     title: "Affiliate Marketing Mastery(AMM)",
     description: "Learn a better strategy for your affiliate marketing journey",
     price: "70.00",
-    image: "https://drive.google.com/file/d/1zvHFRRh6m05nRaUg8yP10pXDcJMiVT1t/preview"
+    image: "https://drive.google.com/file/d/1ADekc-28vhUkXC-ueCymMDWBnl9kWpwr/preview"
  }
 ];
 let currentPage = 1;
